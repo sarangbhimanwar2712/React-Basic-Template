ef34383520419449315630ccf89a99c158c21be9
Sarang <sarangbhimanwar5@gmail.com>
2023-06-12 20:52:45 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Navbar  from "./Components/Navbar";
 import About from "./Components/About";
 import Section from "./Components/Section";
+import Footer from "./Components/Footer";
+
 
 let section_data = [
     {title: "Skills", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim architecto ipsam voluptate soluta sunt suscipit officiis illo at autem sint eos vero provident impedit sequi, deserunt beatae, a eaque dicta ut? Quae sed totam, accusamus facere nemo exercitationem placeat facilis nesciunt ullam animi ipsa quos itaque, perspiciatis quo id quibusdam incidunt debitis qui error tempora soluta alias eius aliquid? Deserunt saepe sequi quos necessitatibus id expedita provident itaque dolore velit pariatur! Minima, ratione. Quisquam, voluptatum."},
@@ -25,7 +27,6 @@ const App = () =>{
         <div>
             <Navbar />
             <About/>
-
             {
                  section_data.map((value,index)=>(
                       <Section 
@@ -33,12 +34,9 @@ const App = () =>{
                         description={value.description}
                       />
                  ))
-
-
             }
-
-        </div>
-        
+            <Footer/>
+        </div>  
     ) ;
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+
+
+const Footer = () =>{
+    return (
+        <div className="footer">
+            <h1>Sarang Sanjay Bhimanwar</h1>
+            {/* <div className="menu"> 
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                    <a href="#">Blog</a>
+            </div> */}
+        </div>
+    ) 
+}
+
+export default Footer ;
